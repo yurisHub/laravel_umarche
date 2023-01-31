e2d1e5bdd17a2e280ce605ea87e7be61ba81f85c
yurisHub <yurie.ngrmz@gmail.com>
2023-01-31 22:16:16 +0900

    変数

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class ComponentTestController extends Controller
 {
     public function showComponent1(){
-        return view('tests.component-test1');
+        $message = 'メッセージ123';
+        return view('tests.component-test1',compact('message'));
     }
     public function showComponent2(){
         return view('tests.component-test2');
