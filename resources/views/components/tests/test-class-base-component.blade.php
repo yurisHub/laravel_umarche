40b6b4d4080f203a386498fb197a3d2e09f459fd
yurisHub <yurie.ngrmz@gmail.com>
2023-02-07 19:12:11 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,5 +1,8 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー2</x-slot>
 コンポーネントテスト2
+<x-test-class-base classBaseMessage="メッセージです" />
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage = "初期値から変更しています" />
     <x-tests.card title="タイトル2" content="本文2" />
+
 </x-tests.app>
