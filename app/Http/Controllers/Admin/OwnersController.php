4b30675bc0e86ca612a7f720287162da12eca69e
yurisHub <yurie.ngrmz@gmail.com>
2023-02-11 19:23:12 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; // Eloquent エロクアント
+use Illuminate\Support\Facades\DB; // QueryBuilder クエリビルダ
+use Carbon\Carbon;
 
 class OwnersController extends Controller
 {
@@ -19,7 +22,26 @@ class OwnersController extends Controller
     }
     public function index()
     {
-        dd('オーナー一覧です');
+        $date_now = Carbon::now();
+        $date_parse = Carbon::parse(now());
+        echo $date_now;
+        echo '<br>';
+        echo $date_now->year;
+        echo '<br>';
+        echo $date_parse;
+
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $q_first = DB::table('owners')->select('name', 'created_at')->first();
+
+        // $c_test = collect([
+        //     'name' => 'テスト'
+        // ]);
+        // var_dump($q_first);
+        // dd($e_all, $q_get, $q_first, $c_test);
+
+        return view ('admin.owners.index', compact('e_all', 'q_get'));
+
     }
 
     /**
