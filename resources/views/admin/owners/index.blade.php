9d9255b8703dfecea1f95f24ca1983ea1465a271
yurisHub <yurie.ngrmz@gmail.com>
2023-02-26 22:19:59 +0900

    pagenation

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -57,6 +57,7 @@
                                 @endforeach
                             </tbody>
                           </table>
+                          {{ $owners->links() }}
                         </div>
                       </div>
                     </section>
