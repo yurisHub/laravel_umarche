0d27e088dcec659b93fc9cbb6e83a3f0cc9e43fc
yurisHub <yurie.ngrmz@gmail.com>
2023-02-27 23:00:41 +0900

    ルート、ビュー編集

--- routes/admin.php
+++ routes/admin.php
@@ -21,9 +21,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.welcome');
+// });
+
+
 
 Route::resource('owners', OwnersController::class)
 ->middleware('auth:admin')->except(['show']);
